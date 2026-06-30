18a5f3bcff4fcb641df25efb6c042d076c87da4e
badr <badr@nixos>
2026-06-30 04:01:50 +0300

    fix0

--- shortcut.nix
+++ shortcut.nix
@@ -1,31 +1,29 @@
-{ ... }:
-
+{ config, pkgs, ... }:
 {
   environment.shellAliases = {
-    # System Rebuild
-    nix-update = "cd /etc/nixos && sudo nix flake update";
+    nix-update  = "cd /etc/nixos && sudo nix flake update";
     nix-rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-    
-    # Fixed spelling: optimize (with a 'z')
-    nix-clean  = "sudo nix-collect-garbage -d && sudo nix store optimise";
-
-    # 
-    nix-shell-stable = "nix shell github:NixOS/nixpkgs/nixos-26.05#legacyPackages.x86_64-linux.";
-
-
-    # Quick Git Commands
+    nix-clean   = "sudo nix-collect-garbage -d && sudo nix store optimise";
     gadd  = "cd /etc/nixos && sudo git add .";
     glog  = "cd /etc/nixos && sudo git log --oneline";
     gpush = "cd /etc/nixos && sudo git push";
     gcm   = "cd /etc/nixos && sudo git commit -am";
   };
 
+  environment.interactiveShellInit = ''
+    nix-shell-stable() {
+      nix shell "github:NixOS/nixpkgs/nixos-26.05#$1"
+    }
+    nix-shell-unstable() {
+      nix shell "nixpkgs#$1"
+    }
+
+  '';
+
   programs.git = {
     enable = true;
     config = {
-      safe = {
-        directory = "/etc/nixos";
-      };
+      safe.directory = "/etc/nixos";
     };
   };
-} 
+}
